7488d08d54c2db26c8cf35cb8d6f3ebcd337f641
Caleb Luster <cwl117@outlook.com>
2024-09-20 09:18:06 -0500

    making progress

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
+import { useAuth } from "@clerk/clerk-expo";
+import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
-    return (
-      <SafeAreaView>
-        <Text>Home</Text>
-      </SafeAreaView>
-    );
+    const { isSignedIn } = useAuth()
+
+    if (isSignedIn) {
+      return <Redirect href="/(root)/(tabs)/home" />
+    }
+
+    return <Redirect href="/(auth)/welcome" />;
 }
 
 export default Home;
